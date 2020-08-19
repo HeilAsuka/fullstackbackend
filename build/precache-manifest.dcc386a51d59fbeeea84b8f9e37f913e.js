273c05292288757006b24ccd099cd93982737f15
XiuQiang Shang <heilasuka911@gmail.com>
2020-08-19 09:16:05 +0800

    add frontend

---
+++ build/precache-manifest.dcc386a51d59fbeeea84b8f9e37f913e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cc80069d5abe996068c2c74f134107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a3ac051f5f2fbdcb7ff",
+    "url": "/static/js/2.891c864b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.891c864b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a863d880e8ffb74c5f30",
+    "url": "/static/js/main.15dc76bc.chunk.js"
+  },
+  {
+    "revision": "c771fc46209c33cf411a",
+    "url": "/static/js/runtime-main.3a2acdf3.js"
+  }
+]);
